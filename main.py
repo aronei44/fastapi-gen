d6b9a6b21d7123eeaa980f51bfdf57a79d991697
aronei44 <meliodasmeliodas224@gmail.com>
2023-01-18 17:34:39 +0700

    docs (main) : add comments

--- main.py
+++ main.py
@@ -3,10 +3,24 @@ from fastapi.middleware.cors import CORSMiddleware
 from config.database import Base, engine
 from routes import authRoute
 
+"""
+Initializing FastApi
+"""
 app = FastAPI()
 
+"""
+For creating table in database.
+Warning, this code always execute migration if something changed in app/models/*.py
+"""
 Base.metadata.create_all(engine)
 
+
+"""
+For enabling Cross Origin Resource Sharing (CORS).
+You can edit allow_origins to your clients target or leave it with '*' if you want to allow all origin.
+You can edit allow_methods to your allowed methods (POST, GET, etc) or leave it with '*' if you want to allow all methods.
+You can edit allow_headers to your allowed headers (Content-Type, Accept, etc) or leave it with '*' if you want to allow all headers.
+"""
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -15,10 +29,18 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+"""
+Test url. you can delete it.
+"""
 @app.get('/api/check')
 async def root():
     return {
         "message": "ok"
     }
 
+"""
+For including your routes file. It can be auto generate by executing command :
+'py gen.py make:route yourroutename'
+Or you can pass it by your self.
+"""
 app.include_router(authRoute.router, prefix="/api/auth", tags=["Authentication"])
